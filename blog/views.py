e4f837b39eec0e9af5574462cac7eed6cd3b338f
Shohjahon1021 <shohjahonberdimurotov21@gmail.com>
2023-02-28 11:03:28 +0500

    projectpages tayyor

---
+++ blog/views.py
@@ -0,0 +1,33 @@
+from django.shortcuts import render
+from django.views.generic import ListView,DetailView,TemplateView
+from .models import Post
+from django.views.generic.edit import CreateView,UpdateView,DeleteView
+from django.urls import reverse_lazy
+# Create your views here.
+class BlogListViews(ListView):
+    model=Post
+    template_name = 'home.html'
+
+class BlogDetaiView(DetailView):
+    model = Post
+    template_name = 'post_detail.html'
+
+class BlogCreateView(CreateView):
+    model = Post
+    template_name = 'post_new.html'
+    fields = ['title','author','body']
+
+class BlogUpdateView(UpdateView):
+    model = Post
+    template_name = 'post_edit.html'
+    fields = ['title','body']
+
+class BlogDeleteView(DeleteView):
+    model = Post
+    template_name = 'post_delete.html'
+    success_url = reverse_lazy('home')
+
+class AboutTemplateView(TemplateView):
+    model = Post
+    template_name = 'about.html'
+
